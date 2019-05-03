795e836be7e41fab44129d512399780276368127
shehand <sonodewcomplex@gmail.com>
2019-05-03 09:38:15 +0530

    add user update api call

--- src/main/java/com/neuroon/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/neuroon/app/ws/service/impl/UserServiceImpl.java
@@ -10,50 +10,54 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.neuroon.app.ws.exceptions.UserServiceException;
 import com.neuroon.app.ws.io.entity.UserEntity;
 import com.neuroon.app.ws.io.repositories.UserRepository;
 import com.neuroon.app.ws.service.UserService;
 import com.neuroon.app.ws.shared.Utils;
 import com.neuroon.app.ws.shared.dto.UserDto;
+import com.neuroon.app.ws.ui.model.response.ErrorMessages;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	Utils utils;
-	
+
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	public UserDto createUser(UserDto user) {
-		
-		if(userRepository.findByEmail(user.getEmail()) != null) throw new RuntimeException("Record already exists");
-		
+
+		if (userRepository.findByEmail(user.getEmail()) != null)
+			throw new RuntimeException("Record already exists");
+
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
-		
+
 		String publicUserId = utils.generateUserId(30);
 		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		userEntity.setUserId(publicUserId);
 
 		UserEntity storedUserDetails = userRepository.save(userEntity);
-		
+
 		UserDto returnValue = new UserDto();
 		BeanUtils.copyProperties(storedUserDetails, returnValue);
-		
+
 		return returnValue;
 	}
-	
+
 	@Override
 	public UserDto getUser(String email) {
 		UserEntity userEntity = userRepository.findByEmail(email);
-		
-		if(userEntity == null) throw new UsernameNotFoundException(email);
-		
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(email);
+
 		UserDto returnValue = new UserDto();
 		BeanUtils.copyProperties(userEntity, returnValue);
 		return returnValue;
@@ -62,21 +66,40 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		UserEntity userEntity = userRepository.findByEmail(email);
-		
-		if(userEntity == null) throw new UsernameNotFoundException(email);
-		
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(email);
+
 		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
 
 	@Override
 	public UserDto getUserByUserId(String userId) {
-		
+
 		UserDto returnValue = new UserDto();
-		
+
 		UserEntity userEntity = userRepository.findByUserId(userId);
-		if(userEntity == null) throw new UsernameNotFoundException(userId);
-		
+		if (userEntity == null)
+			throw new UsernameNotFoundException(userId);
+
 		BeanUtils.copyProperties(userEntity, returnValue);
+		return returnValue;
+	}
+
+	@Override
+	public UserDto updateUser(String userId, UserDto user) {
+		UserDto returnValue = new UserDto();
+
+		UserEntity userEntity = userRepository.findByUserId(userId);
+		if (userEntity == null)
+			throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+
+		userEntity.setFirstName(user.getFirstName());
+		userEntity.setLastName(user.getLastName());
+
+		UserEntity updatedUserDetials = userRepository.save(userEntity);
+		BeanUtils.copyProperties(updatedUserDetials, returnValue);
+		
 		return returnValue;
 	}
 
