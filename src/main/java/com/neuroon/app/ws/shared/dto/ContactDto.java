28579cd489aca085f21fdf7f61d9faa57ecd2a84
shehand <sonodewcomplex@gmail.com>
2019-05-19 16:18:57 +0530

    Add contact data transfer objects getters and setters

--- src/main/java/com/neuroon/app/ws/shared/dto/ContactDto.java
+++ src/main/java/com/neuroon/app/ws/shared/dto/ContactDto.java
@@ -2,4 +2,68 @@ package com.neuroon.app.ws.shared.dto;
 
 public class ContactDto {
 
+	private long id;
+	private String contactId;
+	private String name;
+	private String email;
+	private int number;
+	private String contactType;
+	private UserDto userDetails;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(String contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getContactType() {
+		return contactType;
+	}
+
+	public void setContactType(String contactType) {
+		this.contactType = contactType;
+	}
+
+	public UserDto getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(UserDto userDetails) {
+		this.userDetails = userDetails;
+	}
+
 }
