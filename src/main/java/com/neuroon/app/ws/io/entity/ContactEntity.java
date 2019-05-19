e7e45488a853cbafb8158333bd64a397601eb677
shehand <sonodewcomplex@gmail.com>
2019-05-19 14:57:53 +0530

    Add getters and setters to the contact entity

--- src/main/java/com/neuroon/app/ws/io/entity/ContactEntity.java
+++ src/main/java/com/neuroon/app/ws/io/entity/ContactEntity.java
@@ -30,6 +30,62 @@ public class ContactEntity implements Serializable {
 	@Column(nullable = false, length = 120)
 	private String email;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(String contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContactType() {
+		return contactType;
+	}
+
+	public void setContactType(String contactType) {
+		this.contactType = contactType;
+	}
+
+	public UserEntity getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(UserEntity userDetails) {
+		this.userDetails = userDetails;
+	}
+
 	@Column(nullable = false)
 	private String contactType;
 
