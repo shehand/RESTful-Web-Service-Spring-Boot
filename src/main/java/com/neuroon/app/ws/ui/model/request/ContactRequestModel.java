6dea761bb87acaf7981329652f1f9944eab8da75
shehand <sonodewcomplex@gmail.com>
2019-05-19 14:46:46 +0530

    Add getters and setters to the requser model

--- src/main/java/com/neuroon/app/ws/ui/model/request/ContactRequestModel.java
+++ src/main/java/com/neuroon/app/ws/ui/model/request/ContactRequestModel.java
@@ -2,4 +2,41 @@ package com.neuroon.app.ws.ui.model.request;
 
 public class ContactRequestModel {
 
+	private String name;
+	private int number;
+	private String email;
+	private String contactType;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContactType() {
+		return contactType;
+	}
+
+	public void setContactType(String contactType) {
+		this.contactType = contactType;
+	}
+
 }
