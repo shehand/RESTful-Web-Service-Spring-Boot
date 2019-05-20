ae620f036225f39d8c5cee600fd89c5dd70bc188
shehand <sonodewcomplex@gmail.com>
2019-05-20 09:56:33 +0530

    Add override method to implementation

--- src/main/java/com/neuroon/app/ws/service/impl/ContactServiceImpl.java
+++ src/main/java/com/neuroon/app/ws/service/impl/ContactServiceImpl.java
@@ -1,7 +1,44 @@
 package com.neuroon.app.ws.service.impl;
 
-import com.neuroon.app.ws.service.ContactService;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neuroon.app.ws.io.entity.ContactEntity;
+import com.neuroon.app.ws.io.entity.UserEntity;
+import com.neuroon.app.ws.io.repositories.ContactRepository;
+import com.neuroon.app.ws.io.repositories.UserRepository;
+import com.neuroon.app.ws.service.ContactService;
+import com.neuroon.app.ws.shared.dto.ContactDto;
+
+@Service
 public class ContactServiceImpl implements ContactService {
 
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	ContactRepository contactRepository;
+	
+	@Override
+	public List<ContactDto> getContacts(String userId) {
+
+		List<ContactDto> returnValue = new ArrayList<>();
+		ModelMapper modelMapper = new ModelMapper();
+
+		UserEntity userEntity = userRepository.findByUserId(userId);
+		if (userEntity == null)
+			return returnValue;
+
+		Iterable<ContactEntity> address = contactRepository.findAllByUserDetails(userEntity);
+
+		for (ContactEntity contactEntity : address) {
+			returnValue.add(modelMapper.map(contactEntity, ContactDto.class));
+		}
+
+		return returnValue;
+	}
 }
