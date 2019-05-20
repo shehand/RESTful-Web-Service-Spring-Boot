862925fe3fbdef6c1de21c6c5cabbdd9f8b73a8e
shehand <sonodewcomplex@gmail.com>
2019-05-20 09:56:04 +0530

    Add getContacts by user id method

--- src/main/java/com/neuroon/app/ws/service/ContactService.java
+++ src/main/java/com/neuroon/app/ws/service/ContactService.java
@@ -1,5 +1,11 @@
 package com.neuroon.app.ws.service;
 
+import java.util.List;
+
+import com.neuroon.app.ws.shared.dto.ContactDto;
+
 public interface ContactService {
+
+	List<ContactDto> getContacts(String userId);
 
 }
