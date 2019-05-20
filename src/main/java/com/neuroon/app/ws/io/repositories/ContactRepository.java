f35db550ae16fbbfa721b5dde6ddbf31b91daf7d
shehand <sonodewcomplex@gmail.com>
2019-05-20 09:55:26 +0530

    Add override methods to find contacts

--- src/main/java/com/neuroon/app/ws/io/repositories/ContactRepository.java
+++ src/main/java/com/neuroon/app/ws/io/repositories/ContactRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.neuroon.app.ws.io.entity.AddressEntity;
+import com.neuroon.app.ws.io.entity.ContactEntity;
+import com.neuroon.app.ws.io.entity.UserEntity;
 
 @Repository
 public interface ContactRepository extends CrudRepository<AddressEntity, Long>{
+
+	Iterable<ContactEntity> findAllByUserDetails(UserEntity userEntity);
 
 }
